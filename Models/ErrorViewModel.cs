e840ba021a90e825de1d32e277a7adf06e99d933
Enoch Graham <enochgraham003@outlook.com>
2025-01-21 13:45:27 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TMCP.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
