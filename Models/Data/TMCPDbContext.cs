7e05df2415137a08f8be46e3718b4da7061140b2
Enoch Graham <enochgraham003@outlook.com>
2025-01-23 10:49:13 +0300

    model files

---
+++ Models/Data/TMCPDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace TMCP.Models.Data;
+
+public class TMCPDbContext : IdentityDbContext<User>
+{
+    public TMCPDbContext(DbContextOptions<TMCPDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+}
