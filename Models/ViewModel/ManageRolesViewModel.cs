ee22374706a85192d58773be81af8c6fea99cff1
Enoch Graham <enochgraham003@outlook.com>
2025-01-24 14:51:29 +0300

    Viewmodel files

---
+++ Models/ViewModel/ManageRolesViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace TMCP.Models.ViewModel
+{
+    public class ManageRolesViewModel
+    {
+        public string UserId { get; set; }
+        public string Email { get; set; }
+        public List<RoleViewModel> Roles { get; set; }
+    }
+}
